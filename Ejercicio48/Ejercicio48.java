3f3e6af1bf1fdcfe88017784134515fc23c68a46
daluga0503 <daniel.luquegallardo@gmail.com>
2022-11-04 00:14:13 +0100

    48 added

---
+++ Ejercicio48/Ejercicio48.java
@@ -0,0 +1,45 @@
+package java_capitulo_5.Ejercicio48;
+import java.util.Scanner;
+
+public class Ejercicio48 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca un número entero: ");
+        int num=sc.nextInt();
+        int aux;
+    boolean find;
+
+    System.out.print("Dígitos que aparecen en el número: ");
+    
+    for (int i = 0; i < 10; i++) {
+        find = false;
+        aux = num;
+        while (aux > 0) {
+        if (aux % 10 == i) {
+            find = true;
+        }
+        aux /= 10;
+        }
+        if (find) {
+        System.out.print(i + " ");
+        }
+    }
+
+    System.out.print("\nDígitos que no aparecen: ");
+    
+    for (int i = 0; i < 10; i++) {
+        find = false;
+        aux = num;
+        while (aux > 0) {
+        if (aux % 10 == i) {
+            find = true;
+        }
+        aux /= 10;
+        }
+        if (!find) {
+        System.out.print(i + " ");
+        }
+    }
+
+    }
+}
